6076ba2fa4194e211610e970e7d35aeba429b134
Lawrence Tranter <lawrence.tranter@optus.com.au>
2017-04-10 08:00:32 +1000

    added express to the app, updated others to use

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
 var gulp = require('gulp');
-var less = require('gulp-less');
+var scss = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('less', function(){
-    return gulp.src(['./less/main.less'])
-        .pipe(sourcemaps.init())
-        .pipe(less())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./www/css/styles.css'))
+gulp.task('scss', () => {
+	return gulp.src(['./front/scss/style.scss'])
+		.pipe(sourcemaps.init())
+		.pipe(scss({
+			includePaths: [
+				'./front/components/ux-*/ux-*.scss'
+			]
+		}))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('./public/css/'))
 })
 
-gulp.watch(['./less/**/*.less'], ['less']);
+gulp.task('watch-scss', () => {
+	var files = [
+		'./front/scss/*.scss',
+		'./front/components/ux-*/ux-*.scss'
+	];
+
+	gulp.watch(files, 'scss');
+});
--- server/index.js
+++ server/index.js
@@ -1,4 +1,20 @@
+var bodyParser = require('body-parser')
+
 var express = require('express');
 var app = express();
+var hbs = require('hbs');
 
-app.use()
+//	set hbs as engine
+app.set('view engine', 'hbs');
+app.set('views', './server/views');
+
+app.use(bodyParser.json());
+
+app.use('/', (request, response) => {
+	response
+		.status(200)
+		.render('home.hbs');
+});
+
+app.listen(8080);
+console.log('Listening on:', 8080);
