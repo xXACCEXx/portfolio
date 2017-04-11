60722c719f851ce5879939687ec989950c645f6e
Lawrence Tranter <lawrence.tranter@optus.com.au>
2017-04-11 21:44:13 +1000

    fixed, requires promise resolution

--- utils/template-builder.js
+++ utils/template-builder.js
@@ -6,22 +6,26 @@ var interloper = /(\$\{([a-z$_][a-z0-9$_]+)\})/gi;
 
 module.exports = function (templatePath, destPath, values) {
 	return new Promise((resolve, reject) => {
+		//	for file ops
 		var read_s = fs.createReadStream(templatePath);
 		var write_S = fs.createWriteStream(destPath);
 
-		var instances = [];
+		//	for regex
+		var interlopes = Object.keys(values);
 
 		read_s.on('data', function (chunk) {
-			//	find ${<var>} and track
-			
+			content = chunk.toString();
 
-			//	check values has <var>
-			//	replace ${<var>} with <var> value
+			//	replace ${<var>} with <var> value, for each one found
+			interlopes.forEach(ref => {
+				var customReg = new RegExp('(\\$\\{' + ref + '\\})', 'gi');
+				content = content.replace(customReg, values[ref]);
+			})
 
-			write_S.write()
+			write_S.write(content);
 		});
 
-		read_s.on('error', function (e){
+		read_s.on('error', function (e) {
 			throw e;
 		});
 	});
