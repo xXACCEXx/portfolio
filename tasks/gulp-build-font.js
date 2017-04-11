5db3bc435a323cd50e4b353781334e84c03e9825
Lawrence Tranter <lawrence.tranter@optus.com.au>
2017-04-11 22:08:35 +1000

    removed promise, just require string, values, and return string _w replaced values

--- tasks/gulp-build-font.js
+++ tasks/gulp-build-font.js
@@ -1,13 +1,28 @@
 var simple = require('../utils/stream-simplifier');
+
 var svg2font = require('gulp-svgicons2svgfont');
+var fs = require('fs');
 
 module.exports = function () {
-	return simple(function (content, filename) {
-		svg2font({
-			fontName: 'linear-icons',
-			startUnicode: 0xE800
-		}).on('glyphs', function (glyphs) {
-			console.log('t', glyphs);
+	var writeDest = fs.createWriteStream('./public/fonts/linear-icons.svg', { encoding: 'utf8' });
+
+	// return simple(function (content, filename) {
+	// 	console.log('glyph', content);
+	// 	return content;
+	// })
+
+	return svg2font({
+		fontName: 'linear-icons',
+		startUnicode: 0xE800
+	}).on('glyphs', function (glyphs) {
+		//	glyphs = [{name, unicode, color}];
+
+		var glyphChunks = [];
+
+		glyphs.forEach(glyph => {
+			console.log('glyphs', glyph);
+
+			//	interlope all glyphs with an icon template
 		})
 	})
 }
