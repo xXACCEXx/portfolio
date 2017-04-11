d29b8a3bfb3633ba4624f5aab7d9df1619ecfdb4
Lawrence Tranter <lawrence.tranter@optus.com.au>
2017-04-11 19:11:55 +1000

    fixed the generator, handle some errors and added way to unit test

--- tasks/gulp-create-comp.js
+++ tasks/gulp-create-comp.js
@@ -1,7 +1,12 @@
+var fs = require('fs');
 var exec = require('../utils/child-runner');
 
 module.exports = function (compName) {
 	if (typeof (compName) !== 'string' || compName.trim() == '') throw new Error('component needs a name');
+
+	try { var lstat = fs.lstatSync(`./front/components/ux-${compName}/`); }
+	catch (e) { }
+	if (lstat && lstat.isDirectory()) throw new Error(`component already exists! ${compName}`);
 
 	var tmpl = {};
 
@@ -25,13 +30,15 @@ module.exports = Ractive.extend({
 	//	create folder
 	exec('mkdir -p ./front/components/ux-' + compName)
 		.then(function (logs) {
-			exec()
+			fs.writeFileSync(`./front/components/ux-${compName}/ux-${compName}.js`, tmpl.js);
+			fs.writeFileSync(`./front/components/ux-${compName}/ux-${compName}.hbs`, tmpl.hbs);
+			fs.writeFileSync(`./front/components/ux-${compName}/ux-${compName}.scss`, tmpl.scss);
 		})
 		.catch(err => {
 			console.log('fail', err);
 		})
 }
 
-if(module.parent == undefined) {
+if (module.parent == undefined) {
 	console.log('fasda');
 }
